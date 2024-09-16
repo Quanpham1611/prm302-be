c614c0a591e38e31c451cb5695f6794642f10b6f
QuanPD <i-quanpd@nissho-vn.com>
2024-09-16 17:16:46 +0700

    add product.java

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,5 +1,27 @@
 package com.example.demo.model;
 
-public class Product {
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Table(name = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private String imageLink;
+
+    private String type;
 }
